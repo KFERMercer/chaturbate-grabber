504795dd7051a3c51cf890368a6becfce9e80f10
KFERMercer <iMercer@yeah.net>
2019-08-21 21:53:46 +0800

    大幅提升解析速度; 改善输入合法性检查

--- autoDriving.sh
+++ autoDriving.sh
@@ -7,10 +7,10 @@
 # This is free software, licensed under the GNU General Public License v3.
 #
 # Version: 1.0
-# Release: 1
+# Release: 2
 #
 
-echo "自动挡1.0-1"
+echo "自动挡1.0-2"
 echo "请输入主播名或直播间链接: "
 read input
 
@@ -19,52 +19,59 @@ function inputCheck(){
 	then
 		if [[ ${input} =~ "m.chaturbate" ]]
 		then
-			mLink2link
+			mLink2Link
 		fi
 	else
-		name2link
+		name2Link
 	fi
 }
 
-function mLink2link(){
+function mLink2Link(){
 	input=${input//"m.chaturbate/"/"chaturbate/"}
 }
 
-function name2link(){
+function name2Link(){
 	input="https://chaturbate.com/"${input}"/"
 }
 
 function link2M3u8(){
-	input=$(wget -q -O - ${input}|grep 'initHlsPlayer' | tail -1)
-	if [[ ${input} =~ ".m3u8" ]]
+	if [[ $(curl -4 -s --connect-timeout 5 --head ${input} | head -n 1) =~ "200" ]]
 	then
-		input=${input%"'"*}
-		input=${input#*"'"}
+		input=$(wget -4 -q -O - ${input}|grep 'initHlsPlayer' | tail -1)
+		if [[ ${input} =~ ".m3u8" ]]
+		then
+			input=${input%"'"*}
+			input=${input#*"'"}
+		else
+			echo "主播已下播."
+			exit 0
+		fi
+		echo "获取成功! 直播流为: "
+		echo ${input}
 	else
-		echo "输入不合法, 可能是链接或者主播名错误"
+		echo "链接或主播名错误. "
 		exit 0
 	fi
-	echo "获取成功! 直播流为: "
-	echo ${input}
 }
 
 function hdLink(){
-	input=${input%%playlist.m3u8*}$(wget -q -O - ${input} |tail -1)
+	input=${input%%playlist.m3u8*}$(wget -4 -q -O - ${input} |tail -1)
+	echo "最高清晰度流为: "
 	echo ${input}
 }
 
 function antiCheck(){
-	input=${input//"https://"/"htt删ps://"}
+	input=${input//"http"/"ht删tp"}
 	input=${input//"stream.highwebmedia.com"/"st删ream.highwe删bmedia.co删m"}
+	echo "防和谐最高清晰度流为: "
 	echo ${input}
 }
 
 inputCheck
-echo "获取直播流中: "
+echo "获取直播流中..."
 link2M3u8
-echo "最高清晰度流为: "
+echo "解析清晰度..."
 hdLink
-echo "防和谐最高清晰度流为: "
 antiCheck
 unset input
 exit 0
